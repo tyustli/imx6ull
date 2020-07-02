0e9618eafad12f08e940c9894c105a387d904c27
tyustli <1225613647@qq.com>
2020-07-02 17:56:52 +0800

    添加 libcpu 相关代码

---
+++ libcpu/interrupt.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2006-2018, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2020-07-02     tyustli   first version
+ */
+
+#include <rthw.h>
+#include <rtthread.h>
+
+/* Those varibles will be accessed in ISR, so we need to share them. */
+rt_uint32_t rt_interrupt_from_thread        = 0;
+rt_uint32_t rt_interrupt_to_thread          = 0;
+rt_uint32_t rt_thread_switch_interrupt_flag = 0;
