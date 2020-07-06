2f7ac468dc519d06bfd540bee129ed0e491fefd3
tyustli <1225613647@qq.com>
2020-07-06 16:55:43 +0800

    【添加】 shell 配置(需要停掉 shell 线程)

--- rtconfig.h
+++ rtconfig.h
@@ -31,6 +31,9 @@
 /* Kernel Device Object */
 
 #define RT_USING_DEVICE
+#define RT_USING_CONSOLE
+#define RT_CONSOLEBUF_SIZE 128
+#define RT_CONSOLE_DEVICE_NAME "uart1"
 #define RT_VER_NUM 0x40003
 
 /* RT-Thread Components */
@@ -45,12 +48,28 @@
 
 /* Command shell */
 
+#define RT_USING_FINSH
+#define FINSH_THREAD_NAME "tshell"
+#define FINSH_USING_HISTORY
+#define FINSH_HISTORY_LINES 5
+#define FINSH_USING_SYMTAB
+#define FINSH_USING_DESCRIPTION
+#define FINSH_THREAD_PRIORITY 20
+#define FINSH_THREAD_STACK_SIZE 4096
+#define FINSH_CMD_SIZE 80
+#define FINSH_USING_MSH
+#define FINSH_USING_MSH_DEFAULT
+#define FINSH_ARG_MAX 10
 
 /* Device virtual file system */
 
 
 /* Device Drivers */
 
+#define RT_USING_DEVICE_IPC
+#define RT_PIPE_BUFSZ 512
+#define RT_USING_SERIAL
+#define RT_SERIAL_RB_BUFSZ 64
 
 /* Using USB */
 
@@ -135,5 +154,7 @@
 
 /* Board extended module Drivers */
 
+#define BSP_USING_UART
+#define BSP_USING_UART1
 
 #endif
