0610295f5f14aff5b5d7678976fc5b7405057b85
tyustli <1225613647@qq.com>
2020-08-15 10:14:12 +0800

    【添加】添加 GPIO 中断测试代码

--- applications/main.c
+++ applications/main.c
@@ -16,10 +16,19 @@
 #define LED0_PIN    IMX6ULL_GET_PIN(1, 3) /* GPIO1_IO03 */
 #define KEY0_PIN    IMX6ULL_GET_PIN(1, 18) /* GPIO1_IO18 */
 
+static void key_handler(void)
+{
+    GPIO_ClearPinsInterruptFlags(GPIO1, 1U << 18);
+    rt_kprintf("int\r\n");
+}
+
 int main(int argc, char *argv[])
 {
     rt_pin_mode(LED0_PIN, PIN_MODE_OUTPUT);
     rt_pin_mode(KEY0_PIN, PIN_MODE_INPUT);
+    SystemInstallIrqHandler(GPIO1_Combined_16_31_IRQn, (system_irq_handler_t)(uint32_t)key_handler, NULL);
+    EnableIRQ(GPIO1_Combined_16_31_IRQn);
+    GPIO_EnableInterrupts(GPIO1, 1 << 18);
 
     while(1)
     {
@@ -27,7 +36,6 @@ int main(int argc, char *argv[])
         rt_thread_mdelay(500);
         rt_pin_write(LED0_PIN, PIN_LOW);
         rt_thread_mdelay(500);
-        rt_kprintf("%d\r\n", rt_pin_read(KEY0_PIN));
     }
 
     return 0;
